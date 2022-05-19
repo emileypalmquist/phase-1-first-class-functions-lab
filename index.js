876a39c55c5bd14137b493ab9e66b42732e2d4fc
emileypalmquist <emiley.palmquist@icloud.com>
2022-05-19 09:53:24 -0700

    done: returnLastTwoDrivers

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// create variable with const keyword name should be returnFirstTwoDrivers
-// annonymous function assigned to variable
-
-// define parameter arryOfDrivers
-// function should return first 2 drivers from arryOfDrivers(parameter)
-
 const returnFirstTwoDrivers = (arryOfDrivers) => {
   return arryOfDrivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
 };
