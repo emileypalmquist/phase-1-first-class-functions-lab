3593d7ef2912ea36123b3a90842289979065c687
emileypalmquist <emiley.palmquist@icloud.com>
2022-05-19 09:30:49 -0700

    done: defined returnFristTwoDrivers

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function playMusic(music) {
-  console.log(music);
-}
+// create variable with const keyword name should be returnFirstTwoDrivers
+// annonymous function assigned to variable
+
+// define parameter arryOfDrivers
+// function should return first 2 drivers from arryOfDrivers(parameter)
+
+const returnFirstTwoDrivers = (arryOfDrivers) => {
+  return arryOfDrivers.slice(0, 2);
+};
