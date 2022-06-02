c4923ca22bd6a93a10aa0eb694cd4d123d58d2bf
emileypalmquist <emiley.palmquist@icloud.com>
2022-06-01 20:26:50 -0700

    testingf merge

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-function playMusic(music) {
-  console.log(music);
+const returnFirstTwoDrivers = (arryOfDrivers) => {
+  return arryOfDrivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+// // createFareMultiplier(2)(10)
+// fareDoubler(10);
+
+const potato = [];
+// createFareMultiplier(3)(12)
+
+function selectDifferentDrivers(arryOfDrivers, selectDriversFunction) {
+  return selectDriversFunction(arryOfDrivers);
 }
